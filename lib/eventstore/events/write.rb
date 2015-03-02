62ff6e16ad695de1d739f5636d6823995a7d0d2a
Justin Litchfield <litch@me.com>
2015-03-01 23:12:34 -0600

    Writer is injectable and extracted

---
+++ lib/eventstore/events/write.rb
@@ -0,0 +1,65 @@
+module Eventstore
+  module Events
+    class Write
+      attr_accessor :client
+      attr_accessor :data
+      attr_accessor :id
+      attr_accessor :stream_name
+      attr_accessor :type
+
+      def self.!(params)
+        instance = build(params)
+        instance.!
+      end
+
+      def self.build(params)
+        type = params[:type]
+        data = params[:data].to_json
+        stream_name = params[:stream_name]
+
+        new(type, data, stream_name, client).tap do |instance|
+          instance.id = java.util.UUID.randomUUID().to_s
+        end
+      end
+
+      def self.client
+        @client ||= Vertx::HttpClient.new.tap do |client|
+          p "Initializing Client"
+          client.port = 2113
+          client.host = 'localhost'
+        end
+      end
+
+      def initialize(type, data, stream_name, client)
+        @type = type
+        @data = data
+        @stream_name = stream_name
+        @client = client
+      end
+
+      def !
+        make_request
+      end
+
+      def make_request
+        request = client.post("/streams/#{stream_name}") do |resp|
+          # puts "got response #{resp.status_code}"
+          resp.body_handler do |body|
+            # puts "The total body received was #{body.length} bytes"
+            # puts body
+          end
+        end
+
+        request.put_header('ES-EventType', type)
+        request.put_header("ES-EventId", id)
+        request.put_header('Accept', 'application/vnd.eventstore.atom+json')
+        request.put_header('Content-Length', data.length)
+        request.put_header('Content-Type', 'application/json')
+
+        request.write_str(data)
+
+        request.end
+      end
+    end
+  end
+end
--- main.rb
+++ main.rb
@@ -3,48 +3,12 @@ require 'vertx'
 require 'lib/eventstore/subscriptions/subscribe'
 require 'lib/eventstore/subscriptions/default_handler'
 
-module TestStream
-  class WriteEvent
-    attr_reader :event_data
-    attr_reader :client
-    def self.!
-      new(client).!
-    end
+require 'lib/eventstore/events/write'
 
-    def self.client
-      @client ||= Vertx::HttpClient.new.tap do |client|
-        p "Initializing Client"
-        client.port = 2113
-        client.host = 'localhost'
-      end
-    end
-
-    def initialize(client)
-      @event_data = { "something" => "has data", "value" => rand(100), "time" => Time.now.to_f }.to_json
-      @client = client
-    end
-
-    def !
-      request = client.post('/streams/newstream') do |resp|
-        # puts "got response #{resp.status_code}"
-        resp.body_handler do |body|
-          # puts "The total body received was #{body.length} bytes"
-          # puts body
-        end
-      end
-
-      request.put_header('ES-EventType', 'SomeEvent')
-      request.put_header("ES-EventId", java.util.UUID.randomUUID().to_s)
-      request.put_header('Accept', 'application/vnd.eventstore.atom+json')
-      request.put_header('Content-Length', event_data.length)
-      request.put_header('Content-Type', 'application/json')
-      request.write_str(event_data)
-
-      request.end
-    end
-  end
-end
-
-Vertx.set_periodic(100) { TestStream::WriteEvent.! }
+Vertx.set_periodic(100) {
+  Eventstore::Events::Write.!( type: "EventType",
+                            data: { "something" => "has data", "value" => rand(100), "time" => Time.now.to_f },
+                            stream_name: 'newstream')
+}
 
 Eventstore::Subscriptions::Subscribe.!(starting_point: rand(500), stream: 'newstream', handler: Eventstore::Subscriptions::DefaultHandler)
