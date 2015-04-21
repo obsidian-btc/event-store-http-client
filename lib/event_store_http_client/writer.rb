0505f2c8745b14859ea6e98e2eef94be6f9e621a
Justin Litchfield <litch@me.com>
2015-04-21 14:23:17 -0400

    An instance is of the Writer is configured into receiver, rather than the class

--- lib/event_store_http_client/writer.rb
+++ lib/event_store_http_client/writer.rb
@@ -2,7 +2,7 @@ module EventStore
   module HTTPClient
     module Writer
       def configure(receiver)
-        es_writer = EventStore::HTTPClient::Events::Write
+        es_writer = EventStore::HTTPClient::Events::Write.build
         receiver.es_writer = es_writer
         es_writer
       end
