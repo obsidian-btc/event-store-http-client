19e66b8257f0c4b7b2315ed9e6d99e2d668fa39a
Kelsey Isaacson <kelsey.isaacson@gmail.com>
2015-04-27 14:58:51 -0500

    Remove superfluous requires from init file.

--- init.rb
+++ init.rb
@@ -4,12 +4,4 @@ Bundler.setup
 lib_dir = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift lib_dir unless $LOAD_PATH.include?(lib_dir)
 
-require 'dependency'
-Dependency.activate
-
-require 'logger'
-
-require 'retry'
-p "About to require eventstore"
-require 'event_store_http_client'
-p "Required eventstore"
+require 'event_store/http_client'
